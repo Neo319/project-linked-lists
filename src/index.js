bf045dab3d8caec0724c5117530ea21661838570
CastorDisaster <alexmnevins@gmail.com>
2024-04-08 14:43:08 -0400

    add append, prepend, and size methods

--- src/index.js
+++ src/index.js
@@ -4,29 +4,66 @@ import "./style.css"
 
 
 class LinkedList {
-    LinkedList () { // construct an empty list
+    constructor() {
         this.head = null;
+    }
 
-        function isEmpty() { // returns true if the list is empty
-            return head == null;
-        }
+    isEmpty() { //return true if the list is empty
+        return this.head === null;
+    }
 
-        function prepend(value) { // adds value to the start of the list
-            head = value
+    prepend(value) { //add element to the start of the list
+        if (this.isEmpty()) {
+            this.head = new Node(value, null);
+        } else {
+            let tmp = this.head;
+            this.head = new Node(value, tmp);
         }
+    }
+
+    append(value) { //add node to the end of the list
+        if (this.isEmpty()) {
+            this.head = new Node(value, null);
+        } else {
+            let current = this.head;
+            while (current.next !== null) {
+                current = current.next;
+            }
+            current.next = new Node(value, null);
+        }
+    }
+
+    size() { //return the size of the list
+        let current = this.head; 
+        let counter = 0
+        
+        if (this.isEmpty()) {
+            return 0;
+        } else {
+            counter = 1 //increase to 1 because the head is already counted
+            while (current.next !== null) {
+                current = current.next;
+                counter++;
+            }
+            return counter;
+        }
+        
     }
 }
 
 class Node {
     constructor(data, next) {
-        this.data = data
-        this.next = next
+        this.data = data;
+        this.next = next;
     }
 }
 
+const myList = new LinkedList();
+console.log(myList.isEmpty()); // Output: true
+myList.prepend(10);
+myList.append(20);
+myList.append(30);
+console.log(myList.isEmpty()); // Output: false
+console.log(myList);
 
-
-const myList = new LinkedList 
-
-console.log(myList)
-console.log(myList.isEmpty)
+console.log(myList.size())
