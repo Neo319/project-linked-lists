f8c1017c280cfc9e010af9c47ad1b994ed1517a1
CastorDisaster <alexmnevins@gmail.com>
2024-04-08 13:41:44 -0400

    set up LinkedList class

--- src/index.js
+++ src/index.js
@@ -2,5 +2,31 @@ import dropdown from "./dropdown"
 
 import "./style.css"
 
-console.log("Hello!")
-dropdown()
+
+class LinkedList {
+    LinkedList () { // construct an empty list
+        this.head = null;
+
+        function isEmpty() { // returns true if the list is empty
+            return head == null;
+        }
+
+        function prepend(value) { // adds value to the start of the list
+            head = value
+        }
+    }
+}
+
+class Node {
+    constructor(data, next) {
+        this.data = data
+        this.next = next
+    }
+}
+
+
+
+const myList = new LinkedList 
+
+console.log(myList)
+console.log(myList.isEmpty)
